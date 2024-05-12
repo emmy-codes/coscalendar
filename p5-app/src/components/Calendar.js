aa83a633bfc1d442fb975414f586eae1bdf13e2f
Emma <alunia.moon@gmail.com>
2024-05-12 14:11:47 +0200

    Refactor Calendar component
    Remove some premade logic to introduce date-fns functionality to re-render the calendar without hard coded logic and calculating the calendar month based off:
    
    * Today's date
    * The First and last week of the month based off today
    * The interval between the two weeks/first&last dates

--- p5-app/src/components/Calendar.js
+++ p5-app/src/components/Calendar.js
@@ -7,6 +7,7 @@ import {
     MapPinIcon,
 } from '@heroicons/react/20/solid'
 import { Menu, MenuItem, MenuItems, MenuButton, Transition } from '@headlessui/react'
+import { startOfToday, startOfWeek, endOfMonth, endOfWeek, eachDayOfInterval, format } from 'date-fns'
 
 const meetings = [
     {
@@ -19,60 +20,42 @@ const meetings = [
             'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         location: 'Starbucks',
     },
-    // More meetings...
 ]
-const days = [
-    { date: '2021-12-27' },
-    { date: '2021-12-28' },
-    { date: '2021-12-29' },
-    { date: '2021-12-30' },
-    { date: '2021-12-31' },
-    { date: '2022-01-01', isCurrentMonth: true },
-    { date: '2022-01-02', isCurrentMonth: true },
-    { date: '2022-01-03', isCurrentMonth: true },
-    { date: '2022-01-04', isCurrentMonth: true },
-    { date: '2022-01-05', isCurrentMonth: true },
-    { date: '2022-01-06', isCurrentMonth: true },
-    { date: '2022-01-07', isCurrentMonth: true },
-    { date: '2022-01-08', isCurrentMonth: true },
-    { date: '2022-01-09', isCurrentMonth: true },
-    { date: '2022-01-10', isCurrentMonth: true },
-    { date: '2022-01-11', isCurrentMonth: true },
-    { date: '2022-01-12', isCurrentMonth: true, isToday: true },
-    { date: '2022-01-13', isCurrentMonth: true },
-    { date: '2022-01-14', isCurrentMonth: true },
-    { date: '2022-01-15', isCurrentMonth: true },
-    { date: '2022-01-16', isCurrentMonth: true },
-    { date: '2022-01-17', isCurrentMonth: true },
-    { date: '2022-01-18', isCurrentMonth: true },
-    { date: '2022-01-19', isCurrentMonth: true },
-    { date: '2022-01-20', isCurrentMonth: true },
-    { date: '2022-01-21', isCurrentMonth: true },
-    { date: '2022-01-22', isCurrentMonth: true, isSelected: true },
-    { date: '2022-01-23', isCurrentMonth: true },
-    { date: '2022-01-24', isCurrentMonth: true },
-    { date: '2022-01-25', isCurrentMonth: true },
-    { date: '2022-01-26', isCurrentMonth: true },
-    { date: '2022-01-27', isCurrentMonth: true },
-    { date: '2022-01-28', isCurrentMonth: true },
-    { date: '2022-01-29', isCurrentMonth: true },
-    { date: '2022-01-30', isCurrentMonth: true },
-    { date: '2022-01-31', isCurrentMonth: true },
-    { date: '2022-02-01' },
-    { date: '2022-02-02' },
-    { date: '2022-02-03' },
-    { date: '2022-02-04' },
-    { date: '2022-02-05' },
-    { date: '2022-02-06' },
-]
+
+
+// 
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
 export default function Calendar() {
+
+    // get todays date
+    const todaysDate = startOfToday()
+
+    /**
+     * Use todaysDate to get the first week of the month and the last week of the month to calc date range of the month
+     * to render the calendar. This is because the first week of a month often has dates from the previous month included,
+     * and vice versa for the end of the month
+     */
+
+    const getCalendarMonth = () => {
+        // first day of first week
+        const firstCalendarDay = startOfWeek(todaysDate)
+        // last day of the month
+        const lastDayOfMonth = endOfMonth(todaysDate)
+        // last DATE of calendar month shown
+        const lastCalendarDay = endOfWeek(lastDayOfMonth)
+
+        return eachDayOfInterval({
+            start: firstCalendarDay,
+            end: lastCalendarDay
+        })
+    }
+
     return (
-        <div>
+        <div className="p-20">
             <h2 className="text-base font-semibold leading-6 text-gray-900">Upcoming meetings</h2>
             <div className="lg:grid lg:grid-cols-12 lg:gap-x-16">
                 <div className="mt-10 text-center lg:col-start-8 lg:col-end-13 lg:row-start-1 lg:mt-9 xl:col-start-9">
@@ -103,45 +86,49 @@ export default function Calendar() {
                         <div>S</div>
                     </div>
                     <div className="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
-                        {days.map((day, dayIdx) => (
-                            <button
-                                key={day.date}
-                                type="button"
-                                className={classNames(
-                                    'py-1.5 hover:bg-gray-100 focus:z-10',
-                                    day.isCurrentMonth ? 'bg-white' : 'bg-gray-50',
-                                    (day.isSelected || day.isToday) && 'font-semibold',
-                                    day.isSelected && 'text-white',
-                                    !day.isSelected && day.isCurrentMonth && !day.isToday && 'text-gray-900',
-                                    !day.isSelected && !day.isCurrentMonth && !day.isToday && 'text-gray-400',
-                                    day.isToday && !day.isSelected && 'text-indigo-600',
-                                    dayIdx === 0 && 'rounded-tl-lg',
-                                    dayIdx === 6 && 'rounded-tr-lg',
-                                    dayIdx === days.length - 7 && 'rounded-bl-lg',
-                                    dayIdx === days.length - 1 && 'rounded-br-lg'
-                                )}
-                            >
-                                <time
-                                    dateTime={day.date}
-                                    className={classNames(
-                                        'mx-auto flex h-7 w-7 items-center justify-center rounded-full',
-                                        day.isSelected && day.isToday && 'bg-indigo-600',
-                                        day.isSelected && !day.isToday && 'bg-gray-900'
-                                    )}
+                        {getCalendarMonth().map((day, dayIdx) => {
+                            return (
+                                <button
+                                    key={day.date}
+                                    type="button"
+                                    className='py-1.5 hover:bg-gray-100 focus:z-10'
+                                    // className={classNames(
+                                    //     'py-1.5 hover:bg-gray-100 focus:z-10',
+                                    //     day.isCurrentMonth ? 'bg-white' : 'bg-gray-50',
+                                    //     (day.isSelected || day.isToday) && 'font-semibold',
+                                    //     day.isSelected && 'text-white',
+                                    //     !day.isSelected && day.isCurrentMonth && !day.isToday && 'text-gray-900',
+                                    //     !day.isSelected && !day.isCurrentMonth && !day.isToday && 'text-gray-400',
+                                    //     day.isToday && !day.isSelected && 'text-indigo-600',
+                                    //     dayIdx === 0 && 'rounded-tl-lg',
+                                    //     dayIdx === 6 && 'rounded-tr-lg',
+                                    //     dayIdx === days.length - 7 && 'rounded-bl-lg',
+                                    //     dayIdx === days.length - 1 && 'rounded-br-lg'
+                                    // )}
                                 >
-                                    {day.date.split('-').pop().replace(/^0/, '')}
-                                </time>
-                            </button>
-                        ))}
+                                    
+                                    <time
+                                        dateTime={day.toString()}
+                                        className={classNames(
+                                            'mx-auto flex h-7 w-7 items-center justify-center rounded-full',
+                                            day.isSelected && day.isToday && 'bg-indigo-600',
+                                            day.isSelected && !day.isToday && 'bg-gray-900'
+                                        )}
+                                    >
+                                        {day.toString()}
+                                    </time>
+                                </button>
+                            )
+                        })}
                     </div>
                     <button
                         type="button"
-                        className="mt-8 w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="max-w-xs mt-8 w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Add cosplan
                     </button>
                 </div>
-                <ol className="mt-4 divide-y divide-gray-100 text-sm leading-6 lg:col-span-7 xl:col-span-8">
+                <ol className="mt-4 divide-y divide-gray-100 text-sm leading-6 lg:col-span-7 xl:col-span-8 max-w-xl">
                     {meetings.map((meeting) => (
                         <li key={meeting.id} className="relative flex space-x-6 py-6 xl:static">
                             <img src={meeting.imageUrl} alt="" className="h-14 w-14 flex-none rounded-full" />
