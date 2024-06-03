99b7b2ab185da7dd984d1416ec5e7ff7adaa3c4d
Emma <alunia.moon@gmail.com>
2024-06-03 22:17:45 +0200

    Change Expenses to input fields for future user input

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,63 +1,43 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
+
 export default function Example() {
+
   return (
     <fieldset>
-      <legend className="sr-only">Notifications</legend>
-      <div className="space-y-5">
-        <div className="relative flex items-start">
-
-          <div className="flex h-6 items-center">
-            <input
-              id="cosplan"
-              aria-describedby="cosplan-expense"
-              name="cosplan"
-              type="checkbox"
-              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-            />
-          </div>
-          <div className="ml-3 text-sm leading-6">
-            <label htmlFor="cosplan" className="font-medium text-gray-900">
-              Buy contact cement
-            </label>
-            <p id="cosplan-expense" className="text-gray-500">
-              Get notified when someones posts a comment on a posting.
-            </p>
-          </div>
+      <legend className="sr-only">Cosplay expenses</legend>
+      <div className="flex items-center space-x-3">
+        <input
+          id="cosplan"
+          aria-describedby="cosplan-expense"
+          name="cosplan"
+          type="checkbox"
+          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+        />
+        <label htmlFor="unit-price-input" className="text-gray-500 mr-2 whitespace-nowrap">CosExpense:</label>
+        <input
+          type="text"
+          id="item-name-input"
+          className="font-medium text-gray-900 border rounded-md p-2"
+          placeholder="Item name"
+        />
+        <div className="flex items-center">
+          <label htmlFor="unit-price-input" className="text-gray-500 mr-2 whitespace-nowrap">Price:</label>
+          <input
+            type="number"
+            id="unit-price-input"
+            className="text-gray-900 border rounded-md p-2 w-16"
+            min="0"
+            step="0.01"
+          />
         </div>
-
-        <div className="relative flex items-start">
-          <div className="flex h-6 items-center">
-            <input
-              id="candidates"
-              aria-describedby="candidates-description"
-              name="candidates"
-              type="checkbox"
-              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-            />
-          </div>
-          <div className="ml-3 text-sm leading-6">
-            <label htmlFor="candidates" className="font-medium text-gray-900">
-              Candidates
-            </label>
-            <p id="candidates-description" className="text-gray-500">
-              Get notified when a candidate applies for a job.
-            </p>
-          </div>
-        </div>
+        <label htmlFor="unit-price-input" className="text-gray-500 mr-2 whitespace-nowrap">Link to product(optional):</label>
+        <input
+          type="url"
+          id="product-link-input"
+          className="text-gray-500 border rounded-md p-2"
+          placeholder="Product Link"
+        />
       </div>
     </fieldset>
+
   )
 }
