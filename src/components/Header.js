7f3691f4d702478d3bbfc8e76f567b7b73842718
Emma <alunia.moon@gmail.com>
2024-06-03 22:18:14 +0200

    Start to incorporate Header into project (needs styling)

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <div className="py-5 mx-auto items-center">
+                    
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl lg:mx-0">
+                    <h2 className="flex items-center text-4xl font-bold sm:text-6xl"><span className="sr-only">CosCalendar logo</span>
+                        <img className="h-16 w-auto" src="https://res.cloudinary.com/dcuwctnbv/image/upload/v1717443129/gunuhhigv9mojg4wssaz.png" alt="" />CosCalendar</h2>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                        Cosplay planning and expense tracking
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
