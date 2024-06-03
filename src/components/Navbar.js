5b44c9d9da633d671a3e2ca9e09ab0b387780dbe
Emma <alunia.moon@gmail.com>
2024-06-03 22:18:39 +0200

    Remove references to Tailwind logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
-import { useState } from "react"
-import { Dialog, DialogPanel } from "@headlessui/react"
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
+import { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Header from './Header'
 
 const navigation = [
   { name: "Home", href: "/home" },
-  { name: "Cosplanning", href: "/calendar" },
+  { name: "CosCalendar", href: "/calendar" },
+  { name: "CosPlan", href: "/cosplan" },
   { name: "Expenses", href: "/expenses" },
   { name: "Profile", href: "/user-profile" },
 ]
@@ -16,15 +18,9 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="container mx-auto bg-chetwode-blue-800 text-chetwode-blue-50">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
-          {/* logo to replace */}
-          <a href="/home" className="-m-1.5 p-1.5">
-            <span className="sr-only">CosCalendar logo</span>
-            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-          </a>
-        </div>
+    <div className="container mx-auto bg-chetwode-blue-800 text-chetwode-blue-50">
+      <Header /> <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
+  
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
             <a key={item.name} href={item.href} className="links">
@@ -59,16 +55,9 @@ export default function Navbar() {
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-chetwode-blue-50 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-black">
           <div className="flex items-center gap-x-6">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">CosCalendar Logo</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
+
             <a
-              href="#"
+              href="/signup"
               className="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
             >
               Sign up
@@ -97,7 +86,7 @@ export default function Navbar() {
               </div>
               <div className="py-6">
                 <a
-                  href="#"
+                  href="/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 hover:bg-chetwode-blue-50"
                 >
                   Log in
@@ -107,6 +96,6 @@ export default function Navbar() {
           </div>
         </DialogPanel>
       </Dialog>
-    </header>
+    </div>
   )
 }
