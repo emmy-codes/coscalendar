1751abe11db2377b9fefbf89179d2b2fe459c837
Emma <alunia.moon@gmail.com>
2024-06-06 16:40:46 +0200

    Basic Success component created for reusability

---
+++ src/components/alerts/Success.js
@@ -0,0 +1,35 @@
+import { CheckCircleIcon, XMarkIcon } from '@heroicons/react/20/solid';
+
+function Success() {
+
+    return (
+        isVisible && (
+            <div className="rounded-md bg-orchid-50 p-4">
+                <div className="flex">
+                    <div className="flex-shrink-0">
+                        <CheckCircleIcon
+                            className="h-5 w-5 text-orchid-400"
+                            aria-hidden="true"
+                        />
+                    </div>
+                    <div className="ml-3">
+                        <p className="text-sm font-medium text-orchid-800">Success!</p>
+                    </div>
+                    <div className="ml-auto pl-3">
+                        <div className="-mx-1.5 -my-1.5">
+                            <button
+                                type="button"
+                                onClick={handleDismiss}
+                                className="inline-flex rounded-md bg-orchid-50 p-1.5 text-orchid-500 hover:bg-orchid-100 focus:outline-none focus:ring-2 focus:ring-orchid-600 focus:ring-offset-2 focus:ring-offset-orchid-50"
+                            >
+                                <span className="sr-only">Dismiss</span>
+                                <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>)
+    );
+}
+
+export default Success;
