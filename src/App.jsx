221dc3c465b097942c5b27b297e452490abf0088
Emma <alunia.moon@gmail.com>
2024-06-12 20:59:59 +0200

    Add routes for CosPlanForm/new and ExpensesList

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom"
-import React, { createContext } from 'react'
-import Navbar from './components/Navbar';
-import Calendar from './components/Calendar';
-import CosPlan from './components/CosPlan';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import UserProfile from './components/UserProfile';
-import Expenses from './components/Expenses';
-import Page404 from './components/Page404';
-import Home from './components/Home'
-import LandingPage from "./components/LandingPage";
+import React, { createContext } from "react"
+import Calendar from "./components/Calendar"
+import CosPlan from "./components/CosPlan"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import UserProfile from "./components/UserProfile"
+import Expenses from "./components/Expenses"
+import ExpensesList from "./components/ExpensesList"
+import Page404 from "./components/Page404"
+import Home from "./components/Home"
+import LandingPage from "./components/LandingPage"
+import CosPlanForm from "./components/CosPlanForm"
 
 
 function App() {
@@ -25,8 +26,10 @@ function App() {
       <Route path="/" element={<LandingPage />} >
         <Route path="/home" element={<Home />} />
         <Route path="/cosplan" element={<CosPlan />} />
+        <Route path="/cosplan/new" element={<CosPlanForm />} />
         <Route path="/calendar" element={<Calendar />} />
-        <Route path="/expenses" element={<Expenses />} />
+        {/* <Route path="/expenses" element={<Expenses />} /> */}
+        <Route path="/expenses" element={<ExpensesList/>} />
         <Route path="/user-profile" element={<UserProfile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
