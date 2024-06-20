4cac63f69e43c96321c3e8de3bc1646c5a59db45
Emma <alunia.moon@gmail.com>
2024-06-20 15:28:32 +0200

    UserProfile now better matches the planned input fields (but needs UI upgrades)

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,42 +1,64 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 export default function UserProfile() {
 
     const [username, setUsername] = useState("")
 
     return (
-        <div className="mx-auto container">
+        <div className="mx-auto max-w-4xl bg-orchid-200 p-5 rounded-2xl">
             <div className="px-4 sm:px-0">
                 <h3 className="text-base font-semibold leading-7 text-gray-900">{username}'s Profile</h3>
             </div>
-            <div className="mt-6">
+            <form className="mt-6">
                 <dl className="grid grid-cols-1 sm:grid-cols-2">
                     <div className="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">Name</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">Mother of Dinos</dd>
-                    </div>
-                    <div className="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
                         <dt className="text-sm font-medium leading-6 text-gray-900">Favourite Cosplay</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">Pink Mercy</dd>
-                    </div>
-                    <div className="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">Instagram</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2"><a href="http://www.instagram.com" target='_blank' rel="noreferrer">www.instagram.com</a></dd>
+                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">
+                            <input
+                                type="text"
+                                name="fave_cosplay"
+                                placeholder="What is your favourite cosplay?"
+                                className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            >
+
+                            </input>
+                        </dd>
                     </div>
                     <div className="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
                         <dt className="text-sm font-medium leading-6 text-gray-900">Next convention</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">ComicCon Winter 2024</dd>
+                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2"><input
+                            type="text"
+                            name="next_convention"
+                            placeholder="Keep track of your next convention!"
+                            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        >
+
+                        </input></dd>
                     </div>
                     <div className="border-t border-gray-100 px-4 py-6 sm:col-span-2 sm:px-0">
                         <dt className="text-sm font-medium leading-6 text-gray-900">Bio</dt>
                         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">
-                            Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
-                            qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
-                            pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                            <textarea
+                                type="text"
+                                rows={4}
+                                name="user_bio"
+                                placeholder="Tell us a bit about yourself"
+                                className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            >
+
+                            </textarea>
                         </dd>
                     </div>
                 </dl>
-            </div>
+                <div
+                    className="flex items-center justify-end gap-x-6 border-t border-gray-900 px-4 py-4 sm:px-8"
+                >
+                    <button 
+                    type="submit" 
+                    className="text-gray rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >Save Changes</button>
+                </div>
+            </form>
         </div>
     )
 }
