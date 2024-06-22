2d842a1afe50933539fd0dd8194f6f35e1ecb371
Emma <alunia.moon@gmail.com>
2024-06-22 21:59:42 +0200

    Refactor: Improving layout visibility of homepage which allows to keep the mapping in case of future additions but solves the alignment issues with the grid sizing which was altering the position of the lower image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,11 +19,25 @@ const features = [
     },
 ]
 
-function classNames(...classes) {
-    return classes.filter(Boolean).join(" ")
+
+function FeatureImage(props) {
+    const { feature } = props
+
+    return (<div className="col-span-7">
+        <div className="overflow-hidden rounded-lg bg-gray-100">
+            <img src={feature.imageSrc} alt={feature.imageAlt} className="object-cover object-center max-h-96 w-full" />
+        </div>
+    </div>)
 }
 
+function FeatureText(props) {
+    const { feature } = props
 
+    return (<div className="col-span-5">
+        <h3 className="text-lg font-medium text-gray-900">{feature.name}</h3>
+        <p className="mt-2 text-sm text-gray-500">{feature.description}</p>
+    </div>)
+}
 
 export default function Home() {
     const navigate = useNavigate()
@@ -51,7 +65,7 @@ export default function Home() {
                 <div className="mx-auto max-w-3xl text-center">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Welcome to CosCalendar planning and expenses tracker!</h2>
                     <p className="mt-4 text-gray-500">
-                        Here is the home for staying on top of your cosplays - both keeping track of time with our cosplay calendar, and with expenses 
+                        Here is the home for staying on top of your cosplays - both keeping track of time with our cosplay calendar, and with expenses
                         tracking designed to provide an overview of the costs involved in each cosplay.<br /><a
                             href="/login"
                             className="underline rounded text-base font-semibold leading-7 hover:bg-orchid-200"
@@ -67,25 +81,17 @@ export default function Home() {
                             key={feature.name}
                             className="flex flex-col-reverse lg:grid lg:grid-cols-12 lg:items-center lg:gap-x-8"
                         >
-                            <div
-                                className={classNames(
-                                    featureIdx % 2 === 0 ? "lg:col-start-1" : "lg:col-start-8 xl:col-start-9",
-                                    "mt-6 lg:col-span-5 lg:row-start-1 lg:mt-0 xl:col-span-4"
-                                )}
-                            >
-                                <h3 className="text-lg font-medium text-gray-900">{feature.name}</h3>
-                                <p className="mt-2 text-sm text-gray-500">{feature.description}</p>
-                            </div>
-                            <div
-                                className={classNames(
-                                    featureIdx % 2 === 0 ? "lg:col-start-6 xl:col-start-5" : "lg:col-start-1",
-                                    "flex-auto lg:col-span-7 lg:row-start-1 xl:col-span-8"
-                                )}
-                            >
-                                <div className="overflow-hidden rounded-lg bg-gray-100">
-                                    <img src={feature.imageSrc} alt={feature.imageAlt} className="object-cover object-center max-h-96" />
-                                </div>
-                            </div>
+                            {featureIdx % 2 === 0 ? (
+                                <>
+                                    <FeatureText feature={feature} />
+                                    <FeatureImage feature={feature} />
+                                </>
+                            ) : (
+                                <>
+                                    <FeatureImage feature={feature} />
+                                    <FeatureText feature={feature} />
+                                </>
+                            )}
                         </div>
                     ))}
                 </div>
