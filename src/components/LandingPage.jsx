7624fbfa190ed83672898d6957830078af0587a8
Emma <alunia.moon@gmail.com>
2024-06-22 07:54:31 +0200

    Add navigate so that / redirects to /home only when path = /

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { CurrentUserProvider } from '../contexts/CurrentUserContext'
-import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from "react"
+import { CurrentUserProvider } from "../contexts/CurrentUserContext"
+import Navbar from "./Navbar"
+import { Outlet } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 
 export default function LandingPage() {
+
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (location.pathname == "/") {
+            navigate("/home")
+        }
+    }, [])
 
     return (
         <CurrentUserProvider>
