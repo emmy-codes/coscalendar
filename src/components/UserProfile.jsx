7ad49d253342bde17596151bcaf7f90709a201a8
Emma <alunia.moon@gmail.com>
2024-06-22 10:22:26 +0200

    Add get/put functionality for user Profile input fields and load the current user's username at the top of the page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,61 +1,97 @@
 import { useState, useEffect } from 'react'
+import { useCurrentUser } from '../contexts/CurrentUserContext'
+import { axiosReq } from "../api/axiosDefaults"
 
 export default function UserProfile() {
 
-    const [username, setUsername] = useState("")
+    const currentUser = useCurrentUser()
+    const [userProfile, setUserProfile] = useState({})
+    const [errors, setErrors] = useState({})
 
-    return (
+    const handleSubmitData = (event) => {
+        setUserProfile({
+            ...userProfile,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    useEffect(() => {
+        if (currentUser) {
+            axiosReq.get(
+                `/user_profiles`
+            ).then((response) => setUserProfile(response.data[0]))
+        }
+    }, currentUser)
+
+    const handleProfileSubmit = async (event) => {
+        event.preventDefault()
+        try {
+            console.log("profile:", userProfile)
+            const response = await axiosReq.put(
+                `/user_profiles/${currentUser.pk}/`,
+                userProfile[0]
+            )
+            // updating existing expenses state
+            setUserProfile([response.data])
+            // clears errors if the expense was successfully created
+            setErrors({})
+        } catch (error) {
+            console.error("Error updating profile:", error.response?.data)
+            setErrors(error.response?.data)
+        }
+    }
+
+    return currentUser && (
         <div className="mx-auto max-w-4xl bg-orchid-200 p-5 rounded-2xl">
             <div className="px-4 sm:px-0">
-                <h3 className="text-base font-semibold leading-7 text-gray-900">{username}'s Profile</h3>
+                <h3 className="text-3xl text-center font-bold text-orchid-900 mb-8">{currentUser.username}'s Profile</h3>
             </div>
-            <form className="mt-6">
-                <dl className="grid grid-cols-1 sm:grid-cols-2">
-                    <div className="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">Favourite Cosplay</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">
-                            <input
-                                type="text"
-                                name="fave_cosplay"
-                                placeholder="What is your favourite cosplay?"
-                                className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            >
-
-                            </input>
-                        </dd>
-                    </div>
-                    <div className="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">Next convention</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2"><input
+            <form className="mt-6" onSubmit={handleProfileSubmit}>
+                <dl className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                    <div>
+                        <label htmlFor="fave_cosplay" className="block text-sm font-medium text-gray-700 mb-2">
+                            Favorite Cosplay:
+                        </label>
+                        <input
                             type="text"
-                            name="next_convention"
-                            placeholder="Keep track of your next convention!"
-                            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        >
-
-                        </input></dd>
+                            name="fave_cosplay"
+                            value={userProfile.fave_cosplay}
+                            onChange={handleSubmitData}
+                            className="w-full p-3 border rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-indigo-500"
+                        />
                     </div>
-                    <div className="border-t border-gray-100 px-4 py-6 sm:col-span-2 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">Bio</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">
-                            <textarea
-                                type="text"
-                                rows={4}
-                                name="user_bio"
-                                placeholder="Tell us a bit about yourself"
-                                className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            >
-
-                            </textarea>
-                        </dd>
+                    <div>
+                        <label htmlFor="next_convention" className="block text-sm font-medium text-gray-700 mb-2">
+                                    Next Convention:
+                                </label>
+                                <input
+                                    type="text"
+                                    name="next_convention"
+                                    value={userProfile.next_convention}
+                                    onChange={handleSubmitData}
+                            className="w-full p-3 border rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-indigo-500"
+                                />
+                    </div>
+                    <div className="md:col-span-2">
+                        <label htmlFor="user_bio" className="block text-sm font-medium text-gray-700 mb-2">
+                            Bio:
+                        </label>
+                        <textarea
+                            name="user_bio"
+                            value={userProfile.user_bio}
+                            onChange={handleSubmitData}
+                            rows={4}
+                            className="w-full p-3 border rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-indigo-500"
+                            placeholder="Tell us a bit about yourself"
+                        />
                     </div>
                 </dl>
                 <div
-                    className="flex items-center justify-end gap-x-6 border-t border-gray-900 px-4 py-4 sm:px-8"
+                    className="flex justify-center mt-4"
                 >
-                    <button 
-                    type="submit" 
-                    className="text-gray rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    <button
+                        type="submit"
+                        className="text-gray rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >Save Changes</button>
                 </div>
             </form>
